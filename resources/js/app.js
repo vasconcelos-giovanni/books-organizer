d00422975283b9ebc52c36879609e6937405a3de
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-04-27 21:48:56 -0300

    Add event in save button from book.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+
+//* -------------------------------------------------------------------------- */
+//*                              Book View: create                             */
+//* -------------------------------------------------------------------------- */
+
+const bookForm = document.getElementById('book-form');
+const saveButton = document.getElementById('save-btn');
+const cancelButton = document.getElementById('cancel-btn');
+
+window.addEventListener('load', function () {
+    console.log('Loaded');
+    saveButton.addEventListener(
+        'click',
+        function () {
+            bookForm.submit();
+        }
+    );
+    cancelButton.addEventListener(
+        'onclick',
+        () => {
+            window.location.href = '{{ route(\'books.index\') }}';
+        }
+    );
+});
