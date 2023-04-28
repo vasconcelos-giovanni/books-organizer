74ab97d0273fa5eeae36b3ae70769e7251e4905d
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-04-28 11:24:56 -0300

    Create resource route for Book and test route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('books');
 });
+
+Route::get('/test', [BookController::class, 'test']);
+
+Route::resource('books', BookController::class)
+    ->except('show');
