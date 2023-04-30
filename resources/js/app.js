b30ad164dfe5f3642dfa1498de6913b593571fc0
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-04-30 15:07:14 -0300

    Re-add old JS code after Breeze being installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,33 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+//* -------------------------------------------------------------------------- */
+//*                              Book View: create                             */
+//* -------------------------------------------------------------------------- */
+
+const bookForm = document.getElementById('book-form');
+const deleteBooksForm = document.getElementById('delete-books-form');
+const saveButton = document.getElementById('save-btn');
+const massDeleteButton = document.getElementById('mass-delete-btn');
+
+window.addEventListener('load', function () {
+    console.log('Hello');
+    if (saveButton) {
+        saveButton.addEventListener(
+            'click',
+            function () {
+                bookForm.submit();
+            }
+        );
+    }
+    if (massDeleteButton) {
+        massDeleteButton.addEventListener(
+            'click',
+            function () {
+                deleteBooksForm.submit();
+            }
+        );
+    }
+});
+
