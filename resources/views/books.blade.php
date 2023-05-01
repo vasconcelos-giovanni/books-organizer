d2aa1a9459bfb57b0f3551eb28d4393f1bac9b96
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-04-30 22:33:26 -0300

    Merge frontend with Breeze's

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,57 +1,55 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Livros') }}
+        </h2>
+    </x-slot>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script defer="true" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-    </script>
-    <script src="{{ mix('/resources/js/app.js') }}" type="module" defer="true"></script>
-</head>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
 
-<body>
-    <div class="container">
-        <div class="row p-2">
-            <div class="col-6 d-flex justify-content-start">
-                <h1>Livros</h1>
-            </div>
-            <div class="col-6 d-flex justify-content-end gap-2">
-                <button type="button" class="btn btn-primary"><a href="{{ route('books.create') }}" class="text-reset"
-                        style="text-decoration:none;">Adicionar
-                        livro</a></button>
-                <button type="button" class="btn btn-danger" id="mass-delete-btn">Excluir selecionados</button>
-            </div>
-        </div>
-        <div class="row">
-            <form method="POST" action="{{ route('books.destroy') }}"
-                class="col-12 d-flex justify-content-around flex-wrap gap-4" id="delete-books-form">
-                @csrf
-                @foreach ($books as $book)
-                    <div class="col-3 book d-flex flex-column">
-                        <div class="book-item d-flex align-items-start">
-                            <input type="checkbox" class="delete-checkbox form-check-input me-1" name="ids[]"
-                                value="{{ $book->id }}" />
-                            <a class="text-reset" href="{{ route('books.edit', $book) }}"><img
-                                    class="rounded img-thumbnail"
-                                    src="{{ $book->cover != null ? asset('storage/uploads/' . $book->cover) : 'https://via.placeholder.com/135x220' }}"
-                                    alt="Book Cover" width="135" height="220" /></a>
+                    <div class="container">
+                        <div class="row p-2">
+                            <div class="col-12 d-flex justify-content-end gap-2">
+                                <button type="button" class="btn btn-outline-primary"><a
+                                        href="{{ route('books.create') }}" class="text-reset"
+                                        style="text-decoration:none;">Adicionar
+                                        livro</a></button>
+                                <button type="button" class="btn btn-outline-danger" id="mass-delete-btn">Excluir
+                                    selecionados</button>
+                            </div>
                         </div>
-                        <div>
-                            <ul class="book-details list-unstyled mb-0 ms-3">
-                                <li><strong>SKU:</strong> {{ $book->sku }}</li>
-                                <li><strong>Nome:</strong> {{ $book->name }}</li>
-                                <li><strong>Preço:</strong> R${{ $book->price }}</li>
-                                <li><strong>Peso:</strong> {{ $book->weight }}g</li>
-                            </ul>
+                        <div class="row">
+                            <form method="POST" action="{{ route('books.destroy') }}"
+                                class="col-12 d-flex justify-content-around flex-wrap gap-4" id="delete-books-form">
+                                @csrf
+                                @foreach ($books as $book)
+                                    <div class="col-3 book d-flex flex-column">
+                                        <div class="book-item d-flex align-items-start">
+                                            <input type="checkbox" class="delete-checkbox form-check-input me-1"
+                                                name="ids[]" value="{{ $book->id }}" />
+                                            <a class="text-reset" href="{{ route('books.edit', $book) }}"><img
+                                                    class="rounded img-thumbnail"
+                                                    src="{{ $book->cover != null ? asset('storage/uploads/' . $book->cover) : 'https://via.placeholder.com/135x220' }}"
+                                                    alt="Book Cover" width="135" height="220" /></a>
+                                        </div>
+                                        <div>
+                                            <ul class="book-details list-unstyled mb-0 ms-3">
+                                                <li><strong>SKU:</strong> {{ $book->sku }}</li>
+                                                <li><strong>Nome:</strong> {{ $book->name }}</li>
+                                                <li><strong>Preço:</strong> R${{ $book->price }}</li>
+                                                <li><strong>Peso:</strong> {{ $book->weight }}g</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </form>
                         </div>
                     </div>
-                @endforeach
-            </form>
+                </div>
+            </div>
         </div>
     </div>
-
-</body>
-<html>
+</x-app-layout>
