d88fb68d80fe98318206d7e04f620e6177f6167f
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-08-02 06:29:35 -0300

    style: correct formatting

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -13,54 +13,44 @@
                         <div class="row p-2">
                             <div class="col-12 d-flex justify-content-end gap-2">
                                 <button type="submit" class="btn btn-outline-primary" id="save-btn">Save</button>
-                                <button type="button" class="btn btn-outline-danger" id="cancel-btn"><a
-                                        href="{{ route('books.index') }}" class="text-reset"
-                                        style="text-decoration:none;">Cancel</a></button>
+                                <button type="button" class="btn btn-outline-danger" id="cancel-btn"><a href="{{ url('/') }}" class="text-reset" style="text-decoration:none;">Cancel</a></button>
                             </div>
                         </div>
                         <div class="col-22">
-                            <form method="POST"
-                                action="{{ isset($book) ? route('books.update', $book->id) : route('books.store') }}"
-                                id="book-form" enctype="multipart/form-data">
+                            <form method="POST" action="{{ isset($book) ? route('books.update', $book->id) : route('books.store') }}" id="book-form" enctype="multipart/form-data">
                                 @csrf
                                 @if (isset($book))
-                                    @method('PUT')
+                                @method('PUT')
                                 @endif
                                 <div class="row mb-3">
                                     <label for="inputSKU" class="col-2 col-form-label"><strong>SKU</strong></label>
                                     <div class="col-6">
-                                        <input required="true" type="text" class="form-control" id="inputSKU"
-                                            name="sku" value="{{ isset($book) ? $book->sku : '' }}">
+                                        <input required="true" type="text" class="form-control" id="inputSKU" name="sku" value="{{ isset($book) ? $book->sku : '' }}">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <label for="inputName" class="col-2 col-form-label"><strong>Name</strong></label>
                                     <div class="col-6">
-                                        <input required="true" type="text" class="form-control" id="inputName"
-                                            name="name" value="{{ isset($book) ? $book->name : '' }}">
+                                        <input required="true" type="text" class="form-control" id="inputName" name="name" value="{{ isset($book) ? $book->name : '' }}">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <label for="inputPrice" class="col-2 col-form-label"><strong>Price
                                             ($)</strong></label>
                                     <div class="col-6">
-                                        <input required="true" type="number" step="0.01" class="form-control"
-                                            id="inputPrice" name="price"
-                                            value="{{ isset($book) ? $book->price : '' }}">
+                                        <input required="true" type="number" step="0.01" class="form-control" id="inputPrice" name="price" value="{{ isset($book) ? $book->price : '' }}">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <label for="inputWeight" class="col-2 col-form-label"><strong>Weight
                                             (g)</strong></label>
                                     <div class="col-6">
-                                        <input required="true" type="number" class="form-control" id="inputWeight"
-                                            name="weight" value="{{ isset($book) ? $book->weight : '' }}">
+                                        <input required="true" type="number" class="form-control" id="inputWeight" name="weight" value="{{ isset($book) ? $book->weight : '' }}">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col-2">
-                                        <label for="inputCover"
-                                            class="col-2 col-form-label"><strong>Cover</strong></label>
+                                        <label for="inputCover" class="col-2 col-form-label"><strong>Cover</strong></label>
                                     </div>
                                     <div class="col-6">
                                         <div class="input-group">
@@ -68,15 +58,14 @@
                                                 here</strong></label>
                                             {{-- <label class="input-group-text"
                                 for="inputCover">{{ isset($book) || $book->cover != null ? 'Clique para trocar capa' : 'Clique para inserir capa' }}</strong></label> --}}
-                                            <input type="file" class="form-control-file" id="inputCover"
-                                                accept="image/*" style="display:none;" name="cover">
+                                            <input type="file" class="form-control-file" id="inputCover" accept="image/*" style="display:none;" name="cover">
                                         </div>
                                         {{-- <div class="mt-2">
                             <img src="https://via.placeholder.com/135x220" class="img-thumbnail" alt="Cover">
                         </div> --}}
                                         {{-- <img class="mt-2 rounded img-thumbnail"
                             src="{{ isset($book) ? $book->cover != null ? asset('storage/uploads/' . $book->cover) : 'https://via.placeholder.com/135x220' }}"
-                            alt="Book Cover" width="135" height="220" /> --}}
+                                        alt="Book Cover" width="135" height="220" /> --}}
                                     </div>
                                 </div>
                             </form>
